46301ecc8200035328b80e1273adf38156e0f730
Kenneth Reitz <me@kennethreitz.org>
2019-02-01 09:31:26 -0500

    better repr

--- records_migrate/models.py
+++ records_migrate/models.py
@@ -4,12 +4,9 @@ class Migration:
         self.adapter = adapter
 
     def __repr__(self):
-        return f"<Migration path={self.path!r}>"
+        return f"<Migration path={self.path!r} len={len(self.text)}>"
 
-    def queries(self):
-        for _ in os.walk(self.path):
-            print(_)
-
+    @property
     def text(self):
         """Returns the text of the query."""
         with open(self.path, 'r') as f:
